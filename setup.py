1c367e0ce09cf38d85ce99a583aec24073e5d210
Marcin Celejewski <mcelejewski226225@e-sciance.pl>
2018-05-27 17:12:18 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-    name='python-git-lib',
-    version='1.1',
+    name='bibliotekaPoradnika',
+    version='1.2',
     author='Marcin Celejewski',
     author_email='marcin.celejewski18@gmail.com',
     url='https://github.com/marcincelejewski/DPP-python-lib.git',
