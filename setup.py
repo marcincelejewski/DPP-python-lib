589ef461a29b811ef82c7409bd69e58d31e51f77
Marcin Celejewski <mcelejewski226225@e-sciance.pl>
2018-05-27 18:43:32 +0200

    fix app

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='MyLib4Guide',
-    version='1.4',
+    version='1.5',
     author='Marcin Celejewski',
     author_email='marcin.celejewski18@gmail.com',
     url='https://github.com/marcincelejewski/DPP-python-lib.git',
