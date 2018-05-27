59822976533e533f9a55e909e6bb2cdd70bd755d
Marcin Celejewski <mcelejewski226225@e-sciance.pl>
2018-05-27 17:49:55 +0200

    fix init file

---
+++ libGuide/__init__.py
@@ -0,0 +1,4 @@
+import libGuide.app
+import libGuide.guide
+
+print("Inicializacja")
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-    name='bibliotekaPoradnika',
-    version='1.2',
+    name='MyLib4Guide',
+    version='1.3',
     author='Marcin Celejewski',
     author_email='marcin.celejewski18@gmail.com',
     url='https://github.com/marcincelejewski/DPP-python-lib.git',
