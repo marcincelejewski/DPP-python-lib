0f30bde3914ba2e2bd60b0d28675595f01a87aca
Marcin Celejewski <mcelejewski226225@e-sciance.pl>
2018-05-27 16:30:07 +0200

    add setup.py, manifest

---
+++ setup.py
@@ -0,0 +1,9 @@
+setup(
+    name='python-git-lib',
+    version='1.0',
+    author='Marcin Celejewski',
+    author_email='marcin.celejewski18@gmail.com',
+    url='https://github.com/marcincelejewski/DPP-python-lib.git',
+    packages=['python-git-lib', 'python-git-lib.lib'],
+    
+)
