63f646e28ead410044c977471e0ed4ef5e515d09
Marcin Celejewski <mcelejewski226225@e-sciance.pl>
2018-05-27 16:45:45 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     author='Marcin Celejewski',
     author_email='marcin.celejewski18@gmail.com',
     url='https://github.com/marcincelejewski/DPP-python-lib.git',
-    packages=['python-git-lib', 'python-git-lib.lib'],
+    packages=['lib'],
     
 )
